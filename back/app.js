ba68e8818125755464bb0df58dff2b8522ae7849
Juan Diego Trujillo Montoya <jd.trujillom@uniandes.edu.co>
2020-02-10 01:04:15 -0500

    Readme update and postman collection updated. Maybe last commit :)

--- back/app.js
+++ back/app.js
@@ -4,16 +4,21 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
 const express = require('express');
+const path = require("path");
 const app = express();
-
+const config = require("../front/src/config.json");
 const notesRoute = require("./routes/notes.route");
 
 
+console.log(config.state);
+const port = config.state === "development" ?  config.back_port : config.port;
+
 /* Database conection*/
-const URL_DATABASE = 'mongodb://localhost:27017/challange';
+const URL_DATABASE = config.mongourl;
 mongoose.connect(URL_DATABASE, (err) => {
     console.error.bind(console, 'MongoDB connection error:');
 });
+
 
 /* Middelwares */
 app.use(cors());
@@ -22,7 +27,12 @@ app.use(bodyParser.json());
 
 app.use('/notas', notesRoute);
 
+app.use(express.static(path.join(__dirname, "..", "front", "build")));
+/* Servir los archivos estaticos de React */
+app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "front", "build", "index.html"));
+});
 
-app.listen(3000, () =>{
-    console.log('Example app listening on port 3000!');
+app.listen(port, () =>{
+    console.log('Example app listening on port ' + port);
 })
